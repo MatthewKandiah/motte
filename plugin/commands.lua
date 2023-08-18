d93ce7a56dd773020b3bb6fb77b67ee7a2a9df7e
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-18 15:44:20 +0100

    make a .notes file on command, and delete it on close

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,2 +1,7 @@
--- TODO: app a command Notes which opens an empty buffer (maybe with a set buffer number? or with a random buffer number we'll store? We'll need to interact with it specifically later)
-vim.api.nvim_create_user_command('SayHelloJack', 'lua print(\'Hello Jack\')', {})
+-- On NotesStart we need to:
+--   - open a new notes buffer if one isn't already open
+--   - switch focus to the notes buffer
+vim.api.nvim_create_user_command('NotesStart', function() vim.cmd('vsplit .notes') end, {})
+
+vim.api.nvim_create_autocmd('VimLeavePre', {pattern = '.notes', command = '!rm .notes'})
+
