d3962345e1684ca6ce577ad4db24d67d8c44e8e0
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-18 18:03:33 +0100

    progress

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,7 +1,22 @@
--- On NotesStart we need to:
---   - open a new notes buffer if one isn't already open
---   - switch focus to the notes buffer
-vim.api.nvim_create_user_command('NotesStart', function() vim.cmd('vsplit .notes') end, {})
+NOTES_BUFFER_NUMBER = nil;
 
-vim.api.nvim_create_autocmd('VimLeavePre', {pattern = '.notes', command = '!rm .notes'})
+local function is_notes_buffer_open()
+	print('is_notes_buffer_open')
+	print(NOTES_BUFFER_NUMBER ~= nil)
+	return NOTES_BUFFER_NUMBER ~= nil
+end
 
+local function open_notes_buffer()
+	print('open_notes_buffer')
+	print(NOTES_BUFFER_NUMBER)
+	if not is_notes_buffer_open() then
+		print('open_notes_buffer - opening')
+		vim.cmd('vnew')
+		NOTES_BUFFER_NUMBER = vim.api.nvim_get_current_buf()
+	end
+	print('open_notes_buffer finished')
+end
+
+vim.api.nvim_create_user_command('NotesStart', open_notes_buffer, {})
+
+-- autocommand to close notes buffer if open on leaving vim
