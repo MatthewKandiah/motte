494850b5fd9a7d3d95ec43eeccb65ff1a5827edf
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-19 09:35:06 +0100

    opens non-modifiable buffer once, deletes buffer on window closed

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -8,7 +8,6 @@ local function reset()
 	if NOTES_BUFFER_NUMBER ~= nil then
 		vim.api.nvim_buf_delete(NOTES_BUFFER_NUMBER, { force = true });
 		reset_notes_buffer_number();
-	else
 	end
 end
 
@@ -20,10 +19,10 @@ local function open_notes_buffer()
 	if not is_notes_buffer_open() then
 		vim.cmd('vnew')
 		NOTES_BUFFER_NUMBER = vim.api.nvim_get_current_buf()
+		vim.api.nvim_set_option_value('modifiable', 0, { buf = NOTES_BUFFER_NUMBER })
 	end
 end
 
 vim.api.nvim_create_user_command('NotesStart', open_notes_buffer, {})
 
-vim.api.nvim_create_autocmd('BufLeave', { callback = reset, buffer = NOTES_BUFFER_NUMBER })
-
+vim.api.nvim_create_autocmd('WinClosed', { callback = reset, buffer = NOTES_BUFFER_NUMBER })
