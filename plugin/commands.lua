28b9a7394c28afc09e6aa63cf99df261cb563ab4
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-19 08:31:22 +0100

    think that works for notes that clear when you :q out of the panel

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,22 +1,29 @@
-NOTES_BUFFER_NUMBER = nil;
+local NOTES_BUFFER_NUMBER = nil;
+
+local function reset_notes_buffer_number()
+	NOTES_BUFFER_NUMBER = nil
+end
+
+local function reset()
+	if NOTES_BUFFER_NUMBER ~= nil then
+		vim.api.nvim_buf_delete(NOTES_BUFFER_NUMBER, { force = true });
+		reset_notes_buffer_number();
+	else
+	end
+end
 
 local function is_notes_buffer_open()
-	print('is_notes_buffer_open')
-	print(NOTES_BUFFER_NUMBER ~= nil)
 	return NOTES_BUFFER_NUMBER ~= nil
 end
 
 local function open_notes_buffer()
-	print('open_notes_buffer')
-	print(NOTES_BUFFER_NUMBER)
 	if not is_notes_buffer_open() then
-		print('open_notes_buffer - opening')
 		vim.cmd('vnew')
 		NOTES_BUFFER_NUMBER = vim.api.nvim_get_current_buf()
 	end
-	print('open_notes_buffer finished')
 end
 
 vim.api.nvim_create_user_command('NotesStart', open_notes_buffer, {})
 
--- autocommand to close notes buffer if open on leaving vim
+vim.api.nvim_create_autocmd('BufLeave', { callback = reset, buffer = NOTES_BUFFER_NUMBER })
+
